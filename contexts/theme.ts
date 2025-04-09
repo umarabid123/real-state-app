b26682f0c9703e4fdf959f21b0460b374f53594c
umarabid123 <umarabid709@gmail.com>
2025-04-09 10:41:32 +0500

    changes

--- contexts/theme.ts
+++ contexts/theme.ts
@@ -1,5 +1,7 @@
 export const Colors = {
     primary: '#8BC83F',
     secondary: '#03dac4',
-    white: '#fff'
+    white: '#fff',
+    mediumGray:'background: rgba(83, 88, 122, 1)',
+    lightGray:"rgba(255, 255, 255, 1)"
 }
--- screens/onBoardingScreens/OnBoardingScreen.tsx
+++ screens/onBoardingScreens/OnBoardingScreen.tsx
@@ -8,15 +8,18 @@ import AppButton from '../../components/AppButton/AppButton'
 const OnBoardingScreen = () => {
   return (
     <ImageBackground source={require('../../assets/images/bg-img.png')} style={{ width: '100%', height: '100%' }} resizeMode='cover'>
-      <View style={[globalStyle.container, { backgroundColor: "rgba(33, 98, 138, 0.5)" }]}>
+      <View style={[globalStyle.container, { backgroundColor: "rgba(33, 98, 138, 0.5)" , justifyContent:'space-between', paddingBottom:23}]}>
+        <View />
         <View style={{ alignItems: 'center' }}>
           <Image source={require('../../assets/images/logo.png')} resizeMode='contain' />
           <AppText text='Rise        
         Real Estate' color={Colors.white} fontWeight={700} fontSize={36} style={{ width: 190, textAlign: 'center', marginHorizontal: "auto" }} />
         </View>
 
-        <View>
+        <View style={{marginTop:192}}>
           <AppButton text="let's start" />
+          <AppText text={"Made with love"} style={{textAlign:"center", marginTop:25}} fontSize={10} color={Colors.lightGray} />
+          <AppText text={"v.1.0"} style={{textAlign:"center"}} fontSize={10} fontWeight={700} color={Colors.lightGray} />
         </View>
       </View>
     </ImageBackground>
